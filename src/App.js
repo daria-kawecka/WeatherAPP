f5e4310610ae12a2f6f2432a26f1d84ed403c845
daria-kawecka <dusiek2806@gmail.com>
2021-03-05 21:25:23 +0100

    update main styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import "./App.css";
+import styled from "styled-components";
 import Search from "./Components/Search";
-import Weather from "./Components/Weather";
 import Forecast from "./Components/Forecast";
+import { AppBar } from "./Components/AppBar";
+import img from "./images/background.jpg";
+const AppDiv = styled.div`
+  background: radial-gradient(
+      circle,
+      rgba(68, 90, 128, 0.8) 0%,
+      rgba(34, 45, 64, 0.8) 100%
+    ),
+    url(${img});
+  text-align: center;
+  height: 100vh;
+`;
+
 function App() {
   return (
-    <div className="App">
-      <h1>weather app</h1>
+    <AppDiv>
+      <AppBar></AppBar>
       <Search></Search>
       <Forecast></Forecast>
-    </div>
+    </AppDiv>
   );
 }
 
