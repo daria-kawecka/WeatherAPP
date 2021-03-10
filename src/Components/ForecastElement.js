ac17dccf43e95b3733730a43a3516958baba221a
daria-kawecka <dusiek2806@gmail.com>
2021-03-10 21:17:40 +0100

    add new fields to component

--- src/Components/ForecastElement.js
+++ src/Components/ForecastElement.js
@@ -1,27 +1,34 @@
 import React from "react";
+
 import unixConverter from "./date/UnixConverter";
 import getIcon from "./getIcon";
+import DegToDir from "./degToDir";
+
 import ListGroup from "react-bootstrap/ListGroup";
-import Forecast from "./Forecast";
+
 const ForecastElement = ({ data }) => {
-  console.log(data);
+  DegToDir(data.wind_deg);
   const ForecastData = {
     date: unixConverter(data.dt),
     icon: getIcon(data.weather[0].icon),
-    temp_day: Math.floor(data.temp.day),
-    temp_night: Math.floor(data.temp.night),
+    temp_day: Math.floor(data.temp.day) + "ºC",
+    temp_night: Math.floor(data.temp.night) + "ºC",
     pressure: data.pressure + "hPa",
     humidity: data.humidity + "%",
     wind: data.wind_speed + "m/s",
-    wind_dir: data.wind_deg,
+    wind_dir: DegToDir(data.wind_deg),
   };
   return (
     <ListGroup.Item>
-      <p>{ForecastData.date}</p>
-      <p>{ForecastData.icon}</p>
-      <p>
+      <p className="item__date">{ForecastData.date}</p>
+      <p className="item__icon">{ForecastData.icon}</p>
+      <p className="item__temperature">
         {ForecastData.temp_day}/{ForecastData.temp_night}
       </p>
+      <p className="item__icon">{ForecastData.pressure}</p>
+      <p className="item__humidity">{ForecastData.humidity}</p>
+      <p className="item__wind">{ForecastData.wind}</p>
+      <p className="item__windDir">{ForecastData.wind_dir}</p>
     </ListGroup.Item>
   );
 };
