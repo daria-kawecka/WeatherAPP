498091aad48485b79cce96d4ae67eb2856fd53d5
daria-kawecka <dusiek2806@gmail.com>
2021-03-10 16:07:16 +0100

    add unix converter function

---
+++ src/Components/date/UnixConverter.js
@@ -0,0 +1,14 @@
+import { days } from "./days";
+import { months } from "./months";
+
+export default function convertTime(unix_timestamp) {
+  //from UNIX timestamp:
+  const newDate = new Date(unix_timestamp * 1000);
+
+  let month = months[newDate.getMonth()];
+  let day = days[newDate.getDay()].slice(0, 3);
+  let date = newDate.getDate();
+  //create new date :
+  let time = day + ", " + date + " " + month;
+  return time;
+}
