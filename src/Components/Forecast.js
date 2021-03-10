9d459cdd7e848f550a7f2b55bf9458d040dcd79d
daria-kawecka <dusiek2806@gmail.com>
2021-03-10 17:13:39 +0100

    add forecast component

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -1,7 +1,20 @@
 import React from "react";
-
-const Forecast = () => {
-  return <div>Kilka dni pogoda here</div>;
+import ListGroup from "react-bootstrap/ListGroup";
+import ForecastElement from "./ForecastElement";
+import UnixConverter from "./date/UnixConverter";
+const Forecast = ({ data }) => {
+  console.log(UnixConverter(data[1].dt));
+  return (
+    <ListGroup
+      className="forecast"
+      variant="flush"
+      style={{ height: "100%", backgroundColor: "black" }}
+    >
+      {data.slice(1, 8).map((element, index) => (
+        <ForecastElement data={element} key={index} />
+      ))}
+    </ListGroup>
+  );
 };
 
 export default Forecast;
