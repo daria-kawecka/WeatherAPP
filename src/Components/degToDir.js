b6ef38340685fbd0a31a7b748556803127b1611d
daria-kawecka <dusiek2806@gmail.com>
2021-03-10 20:47:39 +0100

    add degrees to direction converter

---
+++ src/Components/degToDir.js
@@ -0,0 +1,23 @@
+export default function DegToDir(deg) {
+  const directions = [
+    "N",
+    "NNE",
+    "NE",
+    "ENE",
+    "E",
+    "ESE",
+    "SE",
+    "SSE",
+    "S",
+    "SSW",
+    "SW",
+    "WSW",
+    "W",
+    "WNW",
+    "NW",
+    "NNW",
+    "N",
+  ];
+  let val = Math.floor(deg / 22.5);
+  console.log(deg, val, directions[val]);
+}
