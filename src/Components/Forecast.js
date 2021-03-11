8d798e75b58946ab85a80262194ed836455b7df1
daria-kawecka <dusiek2806@gmail.com>
2021-03-11 20:54:09 +0100

    add styles to forecast

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -1,31 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
-import Container from "react-bootstrap/Container";
-
-import ForecastElement from "./ForecastElement";
-import UnixConverter from "./date/UnixConverter";
 import styled from "styled-components";
 
+import ForecastElement from "./ForecastElement";
+import Pagination from "./Pagination";
+
 const StyledList = styled(ListGroup)`
+  padding: 0rem 1rem;
+  height: 100%;
   & .list-group-item {
     display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    background-color: rgba(1, 24, 68, 0.15);
+    color: #e4e2e2;
+    padding: 0.5rem 1rem;
+    height: 100%;
+    & .item__date {
+      color: #c4c4c3;
+      width: 100%;
+    }
+  }
+  .pagination {
+    margin: 7px auto;
+    text-align: center;
+
+    & .page-link {
+      background-color: #c4c4c3;
+      border: none;
+      color: #222d40;
+      line-height: 0.8;
+      font-size: 0.9rem;
+    }
+    .page-item.active .page-link {
+      color: #ffd06e;
+      background-color: #222d40;
+    }
   }
 `;
 
 const Forecast = ({ data }) => {
-  console.log(data[0]);
+  const [currentPage, setCurrentPage] = useState(1); //to set "active page" status
+  const [elementsPerPage, setElementsPerPage] = useState(4);
+
+  const indexOfLastElement = currentPage * elementsPerPage; //last element on page
+  const indexOfFirstElement = indexOfLastElement - elementsPerPage; //first element on page
+  const currentPosts = data.slice(indexOfFirstElement, indexOfLastElement);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
   return (
-    <Container>
-      <StyledList
-        className="forecast"
-        variant="flush"
-        style={{ height: "100%", backgroundColor: "black" }}
-      >
-        {data.map((element, index) => (
-          <ForecastElement data={element} key={index} />
-        ))}
-      </StyledList>
-    </Container>
+    <StyledList className="forecast" variant="flush">
+      {currentPosts.map((element, index) => (
+        <ForecastElement data={element} key={index} />
+      ))}
+      <Pagination currentPage={currentPage} paginate={paginate}></Pagination>
+    </StyledList>
   );
 };
 
--- src/Components/ForecastElement.js
+++ src/Components/ForecastElement.js
@@ -6,8 +6,54 @@ import DegToDir from "./degToDir";
 
 import ListGroup from "react-bootstrap/ListGroup";
 
+import styled from "styled-components";
+import { IconContext } from "react-icons";
+import { WiHumidity } from "react-icons/wi";
+import { WiBarometer } from "react-icons/wi";
+import { WiStrongWind } from "react-icons/wi";
+import { BsCompass } from "react-icons/bs";
+
+const StyledListItem = styled(ListGroup.Item)`
+  p {
+    margin-bottom: 0.3rem;
+  }
+  .icon-main {
+    font-size: 2.9rem;
+  }
+  .icons {
+    font-size: 2rem;
+    height: 90%;
+  }
+  .icon-compass {
+    font-size: 1.2rem;
+    height: 90%;
+    margin-right: 5px;
+  }
+  .item__date {
+    font-size: 0.7rem;
+    text-align: left;
+    margin-bottom: 1px;
+  }
+  .item__icon {
+    width: 100%;
+    margin-bottom: 0px;
+  }
+  .temperature {
+    width: 100%;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    &--day {
+      width: 10%;
+      margin-bottom: 0.2rem;
+      border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+    }
+  }
+`;
+
 const ForecastElement = ({ data }) => {
-  DegToDir(data.wind_deg);
   const ForecastData = {
     date: unixConverter(data.dt),
     icon: getIcon(data.weather[0].icon),
@@ -19,17 +65,42 @@ const ForecastElement = ({ data }) => {
     wind_dir: DegToDir(data.wind_deg),
   };
   return (
-    <ListGroup.Item>
+    <StyledListItem>
       <p className="item__date">{ForecastData.date}</p>
-      <p className="item__icon">{ForecastData.icon}</p>
-      <p className="item__temperature">
-        {ForecastData.temp_day}/{ForecastData.temp_night}
+      <p className="item__icon">
+        <IconContext.Provider value={{ className: "icon-main" }}>
+          {ForecastData.icon}
+        </IconContext.Provider>
       </p>
-      <p className="item__icon">{ForecastData.pressure}</p>
-      <p className="item__humidity">{ForecastData.humidity}</p>
-      <p className="item__wind">{ForecastData.wind}</p>
-      <p className="item__windDir">{ForecastData.wind_dir}</p>
-    </ListGroup.Item>
+      <div className="item__temperature temperature">
+        <p className="temperature--day">{ForecastData.temp_day}</p>
+        <p className="temperature--night">{ForecastData.temp_night}</p>
+      </div>
+      <p className="item__humidity">
+        <IconContext.Provider value={{ className: "icons" }}>
+          <WiHumidity />
+        </IconContext.Provider>
+        {ForecastData.humidity}
+      </p>
+      <p className="item__pressure">
+        <IconContext.Provider value={{ className: "icons" }}>
+          <WiBarometer />
+        </IconContext.Provider>
+        {ForecastData.pressure}
+      </p>
+      <p className="item__wind">
+        <IconContext.Provider value={{ className: "icons" }}>
+          <WiStrongWind />
+        </IconContext.Provider>
+        {ForecastData.wind}
+      </p>
+      <p className="item__windDir">
+        <IconContext.Provider value={{ className: "icon-compass" }}>
+          <BsCompass />
+        </IconContext.Provider>
+        {ForecastData.wind_dir}
+      </p>
+    </StyledListItem>
   );
 };
 
