cb262af6263cc04e115c182322e55b8d295dcb3d
daria-kawecka <dusiek2806@gmail.com>
2021-03-11 20:54:25 +0100

    add pagination component

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Pagination from "react-bootstrap/Pagination";
 
 const PaginationComp = ({ paginate, currentPage }) => {
